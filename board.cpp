9f441b3f9845d3895282345591420ea1e0362e86
JDobrzanski <jakubdobrzanski@icloud.com>
2025-07-28 22:25:48 +0200

    1

---
+++ board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jakub on 28.07.2025.
+//
+
+#include "board.h"
---
+++ board.h
@@ -0,0 +1,16 @@
+//
+// Created by jakub on 28.07.2025.
+//
+
+#ifndef BOARD_H
+#define BOARD_H
+
+
+
+class board {
+
+};
+
+
+
+#endif //BOARD_H
